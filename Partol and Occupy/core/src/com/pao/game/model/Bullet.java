bbccc8cbd53720f55a535e82e717981a77d82555
Jacolek320 <jacekmarkiewicz6@onet.pl>
2024-05-11 13:01:06 +0200

    Bullet

--- Partol and Occupy/core/src/com/pao/game/model/Bullet.java
+++ Partol and Occupy/core/src/com/pao/game/model/Bullet.java
@@ -1,11 +1,34 @@
 package com.pao.game.model;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.pao.game.viewmodel.*;
 
-public class Bullet {
-    int id;
+public class Bullet extends GameObject{
     Board board;
     Color color;
-    void update(long t){}
-    void destroy(){}
+    Bullet(Tank tank){
+        super(tank.getX(), tank.getY(), 3, 5);
+        polygon.setRotation(tank.getRotation());
+        color = tank.getColor();
+    }
+    public void update(long t){
+        final float speed = 0.1f;
+        float angle = polygon.getRotation() * MathUtils.degreesToRadians;
+        float dx = MathUtils.cos(angle) * speed;
+        float dy = MathUtils.sin(angle) * speed;
+        polygon.translate(dx, dy);
+        if(board.checkTankCollision(this) || board.checkBoardCollision(this)) {
+            polygon.translate(-dx, -dy);
+        }
+    }
+    public void destroy(){
+        if(!board.getBulletList().contains(this)){
+            throw new RuntimeException("Bullet is not present in getBulletList()");
+        }
+        board.getBulletList().remove(this);
+    }
+    public Color getColor(){
+        return color;
+    }
 }
