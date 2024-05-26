41563d5565f23fd4b85e1cd687b9e0887799cf5e
Loloekk <karolbie@interia.pl>
2024-05-26 22:38:41 +0200

    delete PPM from MainMenuScreen

--- Patrol-and-Occupy/core/src/com/pao/game/view/MainMenuScreen.java
+++ Patrol-and-Occupy/core/src/com/pao/game/view/MainMenuScreen.java
@@ -15,11 +15,11 @@ import com.pao.game.viewmodel.EditSettings;
 import static com.pao.game.model.Constants.PPM;
 
 public class MainMenuScreen implements Screen {
-    static final float BUTTON_WIDTH = 300 / PPM;
-    static final float BUTTON_HEIGHT = 200 / PPM;
-    static final float PLAY_BUTTON_Y = 550 / PPM;
-    static final float SETTINGS_BUTTON_Y = 350 / PPM;
-    static final float EXIT_BUTTON_Y = 150 / PPM;
+    static final float BUTTON_WIDTH = 300 ;
+    static final float BUTTON_HEIGHT = 200 ;
+    static final float PLAY_BUTTON_Y = 550 ;
+    static final float SETTINGS_BUTTON_Y = 350 ;
+    static final float EXIT_BUTTON_Y = 150 ;
     Drop game;
     OrthographicCamera camera;
     Viewport viewport;
@@ -41,17 +41,17 @@ public class MainMenuScreen implements Screen {
         this.ES = ES;
         //ES = new EditSettings();
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, Drop.WIDTH/PPM, Drop.HEIGHT/PPM);
-        viewport = new ExtendViewport(Drop.WIDTH/PPM, Drop.HEIGHT/PPM, camera);
+        camera.setToOrtho(false, Drop.WIDTH, Drop.HEIGHT);
+        viewport = new ExtendViewport(Drop.WIDTH, Drop.HEIGHT, camera);
         playButtonActive = new Texture(Gdx.files.internal("playActive.png"));
         playButtonInactive = new Texture(Gdx.files.internal("playInactive.png"));
         settingsButtonActive = new Texture(Gdx.files.internal("settingsActive.png"));
         settingsButtonInactive = new Texture(Gdx.files.internal("settingsInactive.png"));
         exitButtonActive = new Texture(Gdx.files.internal("exitActive.png"));
         exitButtonInactive = new Texture(Gdx.files.internal("exitInactive.png"));
-        playButton = new Rectangle(Drop.WIDTH/2/PPM - BUTTON_WIDTH/2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
-        settingsButton = new Rectangle(Drop.WIDTH/2/PPM - BUTTON_WIDTH/2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
-        exitButton = new Rectangle(Drop.WIDTH/2/PPM - BUTTON_WIDTH/2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+        playButton = new Rectangle(Drop.WIDTH/2 - BUTTON_WIDTH/2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+        settingsButton = new Rectangle(Drop.WIDTH/2 - BUTTON_WIDTH/2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+        exitButton = new Rectangle(Drop.WIDTH/2 - BUTTON_WIDTH/2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
         touchPoint = new Vector3();
         painter = new RegionPainter(game.batch,0,0,Drop.WIDTH,Drop.HEIGHT,Drop.WIDTH,Drop.HEIGHT,new Color(0.9f,0.4f,0.4f,1));
     }
@@ -63,38 +63,38 @@ public class MainMenuScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        painter.fillBackground(PPM);
+        painter.fillBackground(1f);
         camera.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
         if(playButton.contains(touchPoint.x, touchPoint.y)) {
-            game.batch.draw(playButtonActive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(playButtonActive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
             if(Gdx.input.isTouched()) {
                 //this.dispose();
                 game.setScreen(new GameScreen(game,ES));
             }
         }
         else {
-            game.batch.draw(playButtonInactive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(playButtonInactive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, PLAY_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
         }
 
         // RegionPainter--------------------------------------------------------------
         if(settingsButton.contains(touchPoint.x, touchPoint.y)) {
-            game.batch.draw(settingsButtonActive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(settingsButtonActive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
             if(Gdx.input.isTouched()) {
                 game.setScreen(new SettingsScreen(game, ES));     //---------------------------------------------------
             }
         }
         else {
-            game.batch.draw(settingsButtonInactive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(settingsButtonInactive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
         }
 
         if(exitButton.contains(touchPoint.x, touchPoint.y)) {
-            game.batch.draw(exitButtonActive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(exitButtonActive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
             if(Gdx.input.isTouched()) {
                 Gdx.app.exit();
             }
         }
         else {
-            game.batch.draw(exitButtonInactive, Drop.WIDTH / 2 / PPM - BUTTON_WIDTH / 2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.batch.draw(exitButtonInactive, Drop.WIDTH / 2  - BUTTON_WIDTH / 2, EXIT_BUTTON_Y - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
         }
         game.batch.end();
     }
