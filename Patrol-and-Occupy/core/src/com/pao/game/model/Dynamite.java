92a7d54451d17382ec081e64aeb65ff3bb8d845d
Jacolek320 <jacekmarkiewicz6@onet.pl>
2024-05-26 21:45:10 +0200

    Dynamite chain explosions

--- Patrol-and-Occupy/core/src/com/pao/game/model/Dynamite.java
+++ Patrol-and-Occupy/core/src/com/pao/game/model/Dynamite.java
@@ -2,6 +2,9 @@ package com.pao.game.model;
 
 import com.badlogic.gdx.physics.box2d.BodyDef;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dynamite extends BodyGameObject {
     Board board;
     ModelSettings settings;
@@ -17,13 +20,24 @@ public class Dynamite extends BodyGameObject {
             return;
         if (!board.getDynamiteList().contains(this))
             throw new RuntimeException("Dynamite is not present in getDynamiteList()");
-        // Boom, destroys every tank in given range
-        for (Tank tank : board.getTankList()) {
-            double distance = Math.sqrt( (this.getX()-tank.getX()) * (this.getX()-tank.getX()) + (this.getY()-tank.getY()) * (this.getY()-tank.getY()) );
-            if(distance <= 300){
-                tank.setIsAlive(false);
-            }
-        }
         board.getDynamiteList().remove(this);
+
+        // Boom, destroys every Tank & Dynamite  in given range
+        for (Tank tank : board.getTankList()) {
+            double distance = Math.sqrt((this.getX() - tank.getX()) * (this.getX() - tank.getX()) + (this.getY() - tank.getY()) * (this.getY() - tank.getY()));
+            if (distance <= 300)
+                tank.setIsAlive(false);
+        }
+        System.err.println("DYNAMITE: " + this);
+        System.err.println("Dynamite list size: " + board.getDynamiteList().size());
+        List<Dynamite> dynamites = new ArrayList<>(board.getDynamiteList());
+        for (Dynamite dynamite : dynamites) {
+            if(!board.getDynamiteList().contains(dynamite))
+                continue;
+            System.err.println(" another dynamite:" + dynamite);
+            double distance = Math.sqrt((this.getX() - dynamite.getX()) * (this.getX() - dynamite.getX()) + (this.getY() - dynamite.getY()) * (this.getY() - dynamite.getY()));
+            if (distance <= 300)
+                dynamite.destroy();
+        }
     }
 }
