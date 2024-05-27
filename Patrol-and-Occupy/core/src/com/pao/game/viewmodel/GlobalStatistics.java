1f7244a6b1ca2e7282b422c0d009265c59cd8c44
Mateusz Gabzdyl <mateusz.gabzdyl@student.uj.edu.pl>
2024-05-27 20:01:57 +0200

    getWinner fixed

--- Patrol-and-Occupy/core/src/com/pao/game/viewmodel/GlobalStatistics.java
+++ Patrol-and-Occupy/core/src/com/pao/game/viewmodel/GlobalStatistics.java
@@ -5,10 +5,7 @@ import com.pao.game.model.ModelPlayer;
 import com.pao.game.model.PlayerStatistics;
 import com.pao.game.model.Tank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GlobalStatistics {
     ViewModel VM;
@@ -40,9 +37,7 @@ public class GlobalStatistics {
     }
     public ModelPlayer getWinner() {
         List<ModelPlayer> players = getPlayers();
-        players.sort((p1, p2) -> Integer.compare(getNumberOfPlates(p1), getNumberOfPlates(p2)));
-        players.sort((p1, p2) -> Integer.compare(getKillNumber(p1), getKillNumber(p2)));
-        players.sort((p1, p2) -> Integer.compare(getDeadNumber(p2), getDeadNumber(p1)));
+        players.sort(Comparator.comparing(this::getNumberOfPlates).thenComparing(this::getKillNumber).thenComparing(Comparator.comparing(this::getDeadNumber).reversed()));
         return players.get(players.size()-1);
     }
 }
