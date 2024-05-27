9d336bbd9ba8c15f076275cf5bb809002c493ec3
Mateusz Gabzdyl <mateusz.gabzdyl@student.uj.edu.pl>
2024-05-27 13:27:51 +0200

    static settings fix

--- Patrol-and-Occupy/core/src/com/pao/game/viewmodel/EditSettings.java
+++ Patrol-and-Occupy/core/src/com/pao/game/viewmodel/EditSettings.java
@@ -4,25 +4,22 @@ import com.pao.game.model.ModelSettings;
 import com.pao.game.model.Options;
 
 public class EditSettings {
-    static ModelSettings settings;
     public static void setOption(Options o, float state)
     {
-        settings.setOption(o,state);
+        ModelSettings.setOption(o,state);
     }
     public static void setOption(Options o, int state)
     {
-        settings.setOption(o,state);
+        ModelSettings.setOption(o,state);
     }
-    public static float getGameTime(){return settings.getGameTime();}
-    public static int getNumberOfPlayers(){return settings.getNumberOfPlayers();}
-    public static int getMap(){return settings.getMap();}
-    public static float getTankSpeed(){return settings.getTankSpeed();}
-    public static float getBulletSpeed(){return settings.getBulletSpeed();}
-    public static int getMagazineCapacity(){return settings.getMagazineCapacity();}
-    public static float getShootCooldown(){return settings.getShootCooldown();}
-    public static float getReceiveCooldown(){return settings.getReceiveCooldown();}
-    public static int getWidth(){return settings.getWidth();}
-    public static int getHeight(){return settings.getHeight();}
-    public static ModelSettings getSettings(){return settings;}
-
+    public static float getGameTime(){return ModelSettings.getGameTime();}
+    public static int getNumberOfPlayers(){return ModelSettings.getNumberOfPlayers();}
+    public static int getMap(){return ModelSettings.getMap();}
+    public static float getTankSpeed(){return ModelSettings.getTankSpeed();}
+    public static float getBulletSpeed(){return ModelSettings.getBulletSpeed();}
+    public static int getMagazineCapacity(){return ModelSettings.getMagazineCapacity();}
+    public static float getShootCooldown(){return ModelSettings.getShootCooldown();}
+    public static float getReceiveCooldown(){return ModelSettings.getReceiveCooldown();}
+    public static int getWidth(){return ModelSettings.getWidth();}
+    public static int getHeight(){return ModelSettings.getHeight();}
 }
